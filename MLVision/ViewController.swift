e3115be772c0be764939fea5d261f41b1d730b86
Vitalik Maksymliuk <makvitalik@gmail.com>
2017-08-25 16:24:16 +0300

    Initial Commit

---
+++ MLVision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLVision
+//
+//  Created by Vitaliy Maksymlyuk on 8/25/17.
+//  Copyright © 2017 Vitaliy Maksymlyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
