d30fb8a9dbce3bfbf1a619a77758b7ddbf803948
Vitalik Maksymliuk <makvitalik@gmail.com>
2017-08-26 20:50:36 +0300

    Added the IBOutlets
    For controlling and present some great content

--- MLVision/Controller/VisionVC.swift
+++ MLVision/Controller/VisionVC.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class VisionVC: UIViewController {
-
+    
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var roundedLblView: RoundedShadowView!
+    @IBOutlet weak var identLbl: UILabel!
+    @IBOutlet weak var confidenceLbl: UILabel!
+    @IBOutlet weak var captureImageView: RoundedShadowImageView!
+    @IBOutlet weak var flashBtn: RoundedShadowButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
